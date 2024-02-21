791b9d51606de9396c04aa766d2a63a2ad2147bd
yrysNM <yrys.n3m@gmail.com>
2024-02-21 18:03:35 +0600

    chor: test deep link

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Linking from 'expo-linking';
+import { StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import { WebView } from 'react-native-webview';
 
+const prefix = Linking.createURL("exp://192.168.1.139:16002/");
 export default function App() {
+  const linking = {
+    prefixes: [prefix],
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
+        <Text>
+
+        Hello World
+        </Text>
+      </NavigationContainer>
   );
 }
 
