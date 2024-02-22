0d4cb01746266a6ed7868441e0d9b9c81da93f64
yrysNM <yrys.n3m@gmail.com>
2024-02-22 18:03:29 +0600

    feat: initial webView postmessage

--- App.tsx
+++ App.tsx
@@ -1,22 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import * as Linking from 'expo-linking';
-import { StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer} from "@react-navigation/native";
+import {SafeAreaView , StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import { WebView } from 'react-native-webview';
+import React, {useEffect, useRef, useState} from "react";
 
-const prefix = Linking.createURL("exp://192.168.1.139:16002/");
 export default function App() {
-  const linking = {
-    prefixes: [prefix],
-  };
+    const webViewEl = useRef<WebView>(null);
+    const info = {token: 'test', user_info: {name: "Alibi", icon: "test"}}
+    const [showWebView, setShowWebView] = useState(false);
+
+    function injectWebViewData() {
+            webViewEl.current?.injectJavaScript(`window.postMessage({token: 'test', user_info: {name: "Alibi", icon: "test"}}, '*')`);
+    }
+
+    if(showWebView) {
+        return  (
+            <View style={{position: "relative", height: "100%", width: "100%"}}>
+
+                    <WebView ref={webViewEl}
+                             style={{height: "100%", width: "100%"}}
+                              onLoadEnd={injectWebViewData}
+                              source={{uri: "http://192.168.1.139:5173/"}}
+                              javaScriptEnabled
+                              onMessage={(event) => {
+                              }}
+                    />
+                <TouchableOpacity style={{position: "absolute", top: 100}} onPress={() => setShowWebView(false)}><Text >Close</Text></TouchableOpacity>
+            </View>
+        )
+    }
 
   return (
-      <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
-        <Text>
-
-        Hello World
-        </Text>
-      </NavigationContainer>
+      <SafeAreaView >
+          <TouchableOpacity style={{justifyContent: "center", alignItems: "center",height: "100%"}} onPress={() => setShowWebView(true)}>
+              <Text>Test WebView</Text>
+          </TouchableOpacity>
+      </SafeAreaView >
   );
 }
 
