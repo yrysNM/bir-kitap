5ce339e47bc83f11c1a68e0d01bcab548809c45c
yrysNM <yrys.n3m@gmail.com>
2024-02-25 18:11:52 +0600

    feat: add provider for date picker

--- src/screens/CreateAccount.tsx
+++ src/screens/CreateAccount.tsx
@@ -5,9 +5,11 @@ import InputItem from "@ant-design/react-native/lib/input-item"
 import DatePicker from "@ant-design/react-native/lib/date-picker"
 import { useState } from "react"
 import { InputStyle } from "../components/InputStyle"
+import Provider from "@ant-design/react-native/lib/provider"
+import List from "@ant-design/react-native/lib/list"
 
 export const CreateAccount = () => {
-    const [dateBirth, setDateBirth] = useState<Date>(new Date())
+    const [dateBirth, setDateBirth] = useState<Date | undefined>(undefined)
     const [info, setInfo] = useState<{ email: string; fullName: string; phone: string; password: string; gender: string; birth: Date }>({
         birth: new Date(),
         email: "",
@@ -20,25 +22,32 @@ export const CreateAccount = () => {
     return (
         <Page>
             <Header isCustomHeader={true} title={"Create an account"} />
-            <View style={{ marginTop: 20, gap: 11 }}>
-                <InputStyle inputTitle={"E-mail"}>
-                    <InputItem type={"text"} style={styles.input} value={info.email} onChange={(value) => setInfo((info) => ({ ...info, email: value }))} placeholder={"example@gmail.com"} />
-                </InputStyle>
-                <InputStyle inputTitle={"Nickname"}>
-                    <InputItem type={"text"} style={styles.input} value={info.fullName} onChange={(value) => setInfo((info) => ({ ...info, fullName: value }))} placeholder={"Jack Jones"} />
-                </InputStyle>
-                <InputStyle inputTitle={"Number phone"}>
-                    <InputItem type={"phone"} style={styles.input} value={info.phone} onChange={(value) => setInfo((info) => ({ ...info, phone: value }))} />
-                </InputStyle>
-                <InputStyle inputTitle={"Password"}>
-                    <InputItem style={styles.input} value={info.password} onChange={(value) => setInfo((info) => ({ ...info, password: value }))} placeholder={"******"} />
+            <Provider>
+                <View style={{ marginTop: 20, gap: 11 }}>
+                    <InputStyle inputTitle={"E-mail"}>
+                        <InputItem type={"text"} style={styles.input} value={info.email} onChange={(value) => setInfo((info) => ({ ...info, email: value }))} placeholder={"example@gmail.com"} />
+                    </InputStyle>
+                    <InputStyle inputTitle={"Nickname"}>
+                        <InputItem type={"text"} style={styles.input} value={info.fullName} onChange={(value) => setInfo((info) => ({ ...info, fullName: value }))} placeholder={"Jack Jones"} />
+                    </InputStyle>
+                    <InputStyle inputTitle={"Number phone"}>
+                        <InputItem type={"phone"} style={styles.input} value={info.phone} onChange={(value) => setInfo((info) => ({ ...info, phone: value }))} />
+                    </InputStyle>
+                    <InputStyle inputTitle={"Password"}>
+                        <InputItem style={styles.input} value={info.password} onChange={(value) => setInfo((info) => ({ ...info, password: value }))} placeholder={"******"} />
 
-                    <Text style={styles.inputExtensionText}>Use at least 8 characters</Text>
-                </InputStyle>
-                <InputStyle inputTitle={"Date of Birth"}>
-                    <DatePicker value={dateBirth} onChange={(value) => setDateBirth(value)} format={"DD-MM-YYYY"} />
-                </InputStyle>
-            </View>
+                        <Text style={styles.inputExtensionText}>Use at least 8 characters</Text>
+                    </InputStyle>
+                    <InputStyle inputTitle={"Date of Birth"}>
+                        <Text>Test</Text>
+                        <Provider>
+                            <DatePicker value={dateBirth} onChange={(value) => setDateBirth(value)} format={"DD-MM-YYYY"}>
+                                <List.Item arrow="horizontal">Select Date</List.Item>
+                            </DatePicker>
+                        </Provider>
+                    </InputStyle>
+                </View>
+            </Provider>
         </Page>
     )
 }
