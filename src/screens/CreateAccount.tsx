7c0f9409850f70af1a42f300f35adc68b5588a5f
yrysNM <yrys.n3m@gmail.com>
2024-02-26 01:35:32 +0600

    feat: add date picker

--- src/screens/CreateAccount.tsx
+++ src/screens/CreateAccount.tsx
@@ -22,32 +22,33 @@ export const CreateAccount = () => {
     return (
         <Page>
             <Header isCustomHeader={true} title={"Create an account"} />
-            <Provider>
-                <View style={{ marginTop: 20, gap: 11 }}>
-                    <InputStyle inputTitle={"E-mail"}>
-                        <InputItem type={"text"} style={styles.input} value={info.email} onChange={(value) => setInfo((info) => ({ ...info, email: value }))} placeholder={"example@gmail.com"} />
-                    </InputStyle>
-                    <InputStyle inputTitle={"Nickname"}>
-                        <InputItem type={"text"} style={styles.input} value={info.fullName} onChange={(value) => setInfo((info) => ({ ...info, fullName: value }))} placeholder={"Jack Jones"} />
-                    </InputStyle>
-                    <InputStyle inputTitle={"Number phone"}>
-                        <InputItem type={"phone"} style={styles.input} value={info.phone} onChange={(value) => setInfo((info) => ({ ...info, phone: value }))} />
-                    </InputStyle>
-                    <InputStyle inputTitle={"Password"}>
-                        <InputItem style={styles.input} value={info.password} onChange={(value) => setInfo((info) => ({ ...info, password: value }))} placeholder={"******"} />
+            <View style={{ marginTop: 20, gap: 11 }}>
+                <InputStyle inputTitle={"E-mail"}>
+                    <InputItem type={"text"} style={styles.input} value={info.email} onChange={(value) => setInfo((info) => ({ ...info, email: value }))} placeholder={"example@gmail.com"} />
+                </InputStyle>
+                <InputStyle inputTitle={"Nickname"}>
+                    <InputItem type={"text"} style={styles.input} value={info.fullName} onChange={(value) => setInfo((info) => ({ ...info, fullName: value }))} placeholder={"Jack Jones"} />
+                </InputStyle>
+                <InputStyle inputTitle={"Number phone"}>
+                    <InputItem type={"phone"} style={styles.input} value={info.phone} onChange={(value) => setInfo((info) => ({ ...info, phone: value }))} />
+                </InputStyle>
+                <InputStyle inputTitle={"Password"}>
+                    <InputItem style={styles.input} value={info.password} onChange={(value) => setInfo((info) => ({ ...info, password: value }))} placeholder={"******"} />
 
-                        <Text style={styles.inputExtensionText}>Use at least 8 characters</Text>
-                    </InputStyle>
-                    <InputStyle inputTitle={"Date of Birth"}>
-                        <Text>Test</Text>
+                    <Text style={styles.inputExtensionText}>Use at least 8 characters</Text>
+                </InputStyle>
+                <InputStyle inputTitle={"Date of Birth"}>
+                    <View style={{ height: 40, flex: 1, width: "100%"}}>
                         <Provider>
-                            <DatePicker value={dateBirth} onChange={(value) => setDateBirth(value)} format={"DD-MM-YYYY"}>
-                                <List.Item arrow="horizontal">Select Date</List.Item>
-                            </DatePicker>
+                            <List>
+                                <DatePicker mode="date" defaultDate={new Date()} value={dateBirth} onChange={(value) => setDateBirth(value)} format={"DD-MM-YYYY"}>
+                                    <List.Item arrow="horizontal">Select Date</List.Item>
+                                </DatePicker>
+                            </List>
                         </Provider>
-                    </InputStyle>
-                </View>
-            </Provider>
+                    </View>
+                </InputStyle>
+            </View>
         </Page>
     )
 }
