2407bf799257af8332a70316a659191dd8831ab9
yrysNM <yrys.n3m@gmail.com>
2024-04-02 06:27:31 +0500

    feat(Recommendation): change  recommendation

---
+++ src/components/ReviewCard.tsx
@@ -0,0 +1,93 @@
+import { View, StyleSheet, Text, Image } from "react-native"
+import { CloudImage } from "./CloudImage"
+import { StarRate } from "./StarRate"
+import UserCustomProfileImg from "../../assets/images/custom-user-profile.jpg"
+import { bookReviewInfo } from "../api/reviewApi"
+
+export const ReviewCard = ({reviewInfo}: {reviewInfo: bookReviewInfo}) => {
+    return (
+        <View style={styles.reviewWrapper}>
+            <CloudImage url={reviewInfo?.book?.imageLink} styleImg={styles.bookReviewImg} />
+            <View style={styles.reviewBookInfo}>
+                <View style={styles.reviewUserInfo}>
+                    <Image style={styles.reviewUserProfileImg} source={UserCustomProfileImg} />
+                    <View style={{ flexShrink: 1 }}>
+                        <Text style={styles.reviewUserName}>{reviewInfo.userName}</Text>
+                        <View style={styles.starBlock}>
+                            <StarRate rateNumber={reviewInfo.rating} />
+                            <Text style={{ fontSize: 9, fontWeight: "600", color: "#7A7878" }}>{reviewInfo.rating.toFixed(0)}</Text>
+                        </View>
+                    </View>
+                </View>
+                <Text style={styles.reviewBookMessage}>{reviewInfo.message}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    reviewWrapper: {
+        borderRadius: 9,
+        backgroundColor: "#FFFFFF",
+        shadowColor: "rgba(0, 0, 0, 0.25)",
+        shadowOffset: {
+            width: 0,
+            height: 0
+        },
+        shadowRadius: 6,
+        elevation: 6,
+        shadowOpacity: 1,
+        marginTop: 1,
+        marginHorizontal: 5,
+        marginBottom: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 19,
+        flexDirection: "row",
+        gap: 14,
+        alignItems: "flex-start",
+    },
+
+    bookReviewImg: {
+        width: 84,
+        height: 120,
+        borderRadius: 9,
+        objectFit: "cover",
+    },
+
+    reviewBookInfo: {
+        flex: 1,
+        flexDirection: "column",
+        gap: 5,
+    },
+
+    reviewBookMessage: {
+        fontSize: 7,
+        fontWeight: "600",
+        lineHeight: 10,
+    },
+
+    reviewUserInfo: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 8,
+    },
+
+    reviewUserProfileImg: {
+        width: 31,
+        height: 31,
+        borderRadius: 500,
+    },
+
+    reviewUserName: {
+        fontSize: 11,
+        fontWeight: "600",
+        lineHeight: 15,
+        color: "#000000",
+    },
+
+    starBlock: {
+        flexDirection: "row",
+        gap: 5,
+        alignItems: "center",
+    },
+})
--- src/screens/Recommendations.tsx
+++ src/screens/Recommendations.tsx
@@ -1,14 +1,32 @@
 import { StyleSheet, TouchableOpacity, Text, Dimensions } from "react-native"
 import { Page } from "../layouts/Page"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Tabs from "@ant-design/react-native/lib/tabs"
 import { TabBarPropsType } from "@ant-design/react-native/lib/tabs/PropsType"
 import Carousel from "react-native-snap-carousel"
 import { Header } from "../components/Header"
 import View from "@ant-design/react-native/lib/view"
+import { bookInfo } from "../api/bookApi"
+import { RecommendationAPI } from "../api/recommendationApi"
+import { BookCard } from "../components/BookCard"
+import { NoData } from "../components/NoData"
+import { bookReviewInfo } from "../api/reviewApi"
+import { postInfo } from "../api/postApi"
+import { IUserInfo } from "../api/authApi"
+import { ReviewCard } from "../components/ReviewCard"
 
 export const Recommendations = () => {
-    const [recommendationType, setRecommendationType] = useState<string>("")
+    const { fetchData: fetchBookData } = RecommendationAPI("books")
+    const { fetchData: fetchReviewsData } = RecommendationAPI("reviews")
+    const { fetchData: fetchPostsData } = RecommendationAPI("posts")
+    const { fetchData: fetchUsersData } = RecommendationAPI("users")
+
+    const [recommendationType, setRecommendationType] = useState<string>("Books")
+    const [bookDataList, setBookDataList] = useState<bookInfo[]>([])
+    const [reviewDataList, setReviewDataList] = useState<bookReviewInfo[]>([])
+    const [postDataList, setPostDataList] = useState<postInfo[]>([])
+    const [userDataList, setUserDataList] = useState<IUserInfo[]>([])
+
     const [tabs] = useState<{ title: string }[]>([
         {
             title: "Books",
@@ -23,6 +41,34 @@ export const Recommendations = () => {
             title: "Users",
         },
     ])
+
+    useEffect(() => {
+        if (recommendationType === "Books") {
+            fetchBookData({}).then((res) => {
+                if (res.result_code === 0) {
+                    setBookDataList(res.data)
+                }
+            })
+        } else if (recommendationType === "Reviews") {
+            fetchReviewsData({}).then((res) => {
+                if (res.result_code === 0) {
+                    setReviewDataList(JSON.parse(JSON.stringify(res.data)))
+                }
+            })
+        } else if (recommendationType === "Posts") {
+            fetchPostsData({}).then((res) => {
+                if (res.result_code === 0) {
+                    setPostDataList(JSON.parse(JSON.stringify(res.data)))
+                }
+            })
+        } else {
+            fetchUsersData({}).then((res) => {
+                if (res.result_code === 0) {
+                    setUserDataList(JSON.parse(JSON.stringify(res.data)))
+                }
+            })
+        }
+    }, [recommendationType])
 
     const isSelectRecommendationType = (type: string) => {
         return recommendationType === type
@@ -56,22 +102,53 @@ export const Recommendations = () => {
     return (
         <Page>
             <Header isCustomHeader={false} title="Recommendations" isGoBack />
-            <View style={{ marginTop: 20 }}>
-                <Tabs tabs={tabs} swipeable={false} renderTabBar={(tabProps) => tabHeader(tabProps)}></Tabs>
+            <View style={{ flex: 1, height: "auto" }}>
+                <Tabs tabs={tabs}  swipeable={false} renderTabBar={(tabProps) => tabHeader(tabProps)} initialPage="Books"  animated={false}>
+                    {/* books */}
+                    <View>
+                        <View style={{ marginTop: 30 }}>
+                            <View style={styles.bookWrapper}>{bookDataList.length ? bookDataList.map((book) => <BookCard key={book.id} bookInfo={book} />) : <NoData />}</View>
+                        </View>
+                    </View>
+                    {/* reviews */}
+                    <View>
+                        <View style={{ marginTop: 30 }}>
+                            <View style={styles.reviewWrapper}>{reviewDataList.length ? reviewDataList.map((review) => <ReviewCard key={review.id} reviewInfo={review} />) : <NoData />}</View>
+                        </View>
+                    </View>
+                    {/* posts */}
+                    <View></View>
+                    {/* users */}
+                    <View></View>
+                </Tabs>
             </View>
         </Page>
     )
 }
 
 const styles = StyleSheet.create({
+    bookWrapper: {
+        flexWrap: "wrap",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 25,
+        marginBottom: 30,
+    },
+    reviewWrapper: {
+        flexDirection: "column",
+        gap: 17,
+        justifyContent: "center",
+        alignContent: "center",
+    },
     tabHeadBlock: {
         paddingVertical: 8,
         borderRadius: 10,
         marginRight: 10,
-        position: "absolute",
-        top: -10,
-        left: 0,
         backgroundColor: "#fff",
+        top: 10,
+        width: "100%",
+        height: 50,
     },
     bookBlock: {
         paddingVertical: 8,
