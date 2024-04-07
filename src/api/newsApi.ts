7fef260a822c7056d2b18fe8d2257e6102f95735
Nurdaulet123456 <nurdauletdeveloper@gmail.com>
2024-04-07 20:43:32 +0600

    newsapi

--- src/api/newsApi.ts
+++ src/api/newsApi.ts
@@ -1,21 +1,11 @@
 import useApi from "../hook/useApi"
 
-export type newsInfo = {
-    id: string
-    title: string
-    content: string
-    imageLink: string
-    verticalImage: string | null
-    createtime: number
-}
 
 interface INews extends IResponse {
-    data: newsInfo[]
+    data: string[]
 }
 
 export function NewsApi(url: string, method: string = "POST") {
-
-
     const { res, fetchData } = useApi<INews>(`news/${url}`, method)
 
     return {
