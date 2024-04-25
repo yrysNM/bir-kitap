7a5ebf50677ee3cef6f8df881a32fc81a36b5a44
yrysNM <yrys.n3m@gmail.com>
2024-04-25 06:51:32 +0500

    feat(Follow, BookDetail, ReadersUser SearchInut): add search and genres

--- src/screens/BookDetail.tsx
+++ src/screens/BookDetail.tsx
@@ -107,6 +107,7 @@ export const BookDetail = () => {
                 <CloudImage styleImg={styles.bookImg} url={bookInfo?.book.imageLink} />
                 <StarRate rateNumber={bookInfo?.customInfo.rating || 0} size={25} />
                 <Text style={styles.bookTitle}>{bookInfo?.book.title}</Text>
+                <Text style={[styles.bookAuthor, {color: "#212121", fontSize: 15}]}>{bookInfo?.book.genres.join(", ")}</Text>
                 <Text style={styles.bookAuthor}>{bookInfo?.book.author}</Text>
 
                 <View style={styles.statisticWrapper}>
