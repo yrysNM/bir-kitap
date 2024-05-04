139b8c42b123631cfb9b4e10840e8d35aabeecc1
yrysNM <yrys.n3m@gmail.com>
2024-05-04 06:47:56 +0500

    feat(PostDetail): change items

--- src/screens/PostDetail.tsx
+++ src/screens/PostDetail.tsx
@@ -7,14 +7,17 @@ import { CloudImage } from "../components/CloudImage"
 export const PostDetail = () => {
     const attachements = ["https://cdn.pixabay.com/photo/2022/12/01/04/35/sunset-7628294_640.jpg", "https://img.freepik.com/free-photo/beautiful-anime-character-cartoon-scene_23-2151035157.jpg"]
 
-    const _renderItem = ({ item }: { item: string }) => {
-        return <CloudImage url={item} styleImg={styles.postImages} />
+    const _renderItemPostImage = ({ item }: { item: string }) => {
+        return (
+            <View>
+                <CloudImage url={item} styleImg={styles.postImages} />
+            </View>
+        )
     }
-
     return (
         <Page>
             <Header isGoBack title="" />
-            <View style={styles.postWrapper}>{attachements.length !== 1 ? <Carousel sliderWidth={Dimensions.get("window").width} renderItem={_renderItem} itemWidth={170} dataList={attachements} layout={"default"} currentIndex={attachements.length === 2 ? 0 : 1} loop /> : null}</View>
+            <View style={styles.postWrapper}>{attachements.length !== 1 ? <Carousel dataList={attachements} renderItem={_renderItemPostImage} sliderWidth={Dimensions.get("window").width} itemWidth={170} layout={"default"} currentIndex={1} loop /> : <Text>Test</Text>}</View>
             <View>
                 <Text>Test book</Text>
             </View>
