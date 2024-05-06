e4e51f309a18127fc09750c645aac1ac792ae9a2
yrysNM <yrys.n3m@gmail.com>
2024-05-06 18:11:03 +0500

    feat(Loading, Clubs): change Clubs and loading

--- src/screens/services/Clubs.tsx
+++ src/screens/services/Clubs.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View, StyleSheet, TouchableOpacity, FlatList } from "react-native"
+import { Image, Text, View, StyleSheet, TouchableOpacity, FlatList, Dimensions } from "react-native"
 import { CloudImage } from "../../components/CloudImage"
 import { Page } from "../../layouts/Page"
 import { useEffect, useState } from "react"
@@ -16,13 +16,23 @@ import { ClubAPI, clubInfo } from "../../api/clubApi"
 import Toast from "@ant-design/react-native/lib/toast"
 import EditImg from "../../../assets/images/edit.png"
 import TrashImg from "../../../assets/images/trash.png"
+import { useAppSelector } from "../../hook/useStore"
+import { Loading } from "../../components/Loading"
+import LockImg from "../../../assets/images/lock.png"
 
 export const Clubs = () => {
     const { fetchData: fetchMyClubData } = ClubAPI("my/list")
     const { fetchData: fetchClubData } = ClubAPI("list")
     const { fetchData: fetchCreateClubData } = ClubAPI("create")
+    const { fetchData: fetchUpdateClubData } = ClubAPI("update")
+    const { fetchData: fetchDeleteClubData } = ClubAPI("delete")
+    const { isLoading } = useAppSelector((state) => state.mainSlice)
     // const screenWidth = Dimensions.get("window").width
     // const screenHeight = Dimensions.get("window").height
+    // const [position, setPosition] = useState<{ x: number; y: number }>({
+    //     x: screenWidth - 80,
+    //     y: screenHeight - 80,
+    // })
     const tabs = [
         {
             label: "Clubs",
@@ -31,14 +41,23 @@ export const Clubs = () => {
         { label: "My clubs", value: "my_clubs" },
     ]
     const [tab, setTab] = useState<string>("clubs")
-    // const [position, setPosition] = useState<{ x: number; y: number }>({
-    //     x: screenWidth - 80,
-    //     y: screenHeight - 80,
-    // })
     const [clubInfo, setClubInfo] = useState<{ title: string; isPrivate: boolean }>({ title: "", isPrivate: false })
     const [myClubList, setMyClubList] = useState<clubInfo[]>([])
     const [clubList, setClubList] = useState<clubInfo[]>([])
-    const [showAddClubModal, setShowAddClubModal] = useState<boolean>(false)
+    const [showAddClubModal, setShowAddClubModal] = useState<{
+        id: string
+        isOpen: boolean
+    }>({
+        id: "",
+        isOpen: false,
+    })
+    const [showWarningModal, setShowWarningModal] = useState<{
+        id: string
+        isOpen: boolean
+    }>({
+        id: "",
+        isOpen: false,
+    })
 
     useEffect(() => {
         loadData()
@@ -92,14 +111,66 @@ export const Clubs = () => {
     //     }
     // }
 
+    const onDeleteClub = () => {
+        if (!showWarningModal.id) return
+
+        fetchDeleteClubData({
+            id: showWarningModal.id,
+        }).then((res) => {
+            if (res.result_code === 0) {
+                Toast.success("Successfuly deleted post")
+                loadData()
+                onCloseModal(true)
+            }
+        })
+    }
+
     const onCreateClub = () => {
         fetchCreateClubData({ ...clubInfo, isPrivate: Number(clubInfo.isPrivate) }).then((res) => {
             if (res.result_code === 0) {
                 Toast.success("Successfuly created post")
+                loadData()
                 setClubInfo({ title: "", isPrivate: false })
-                setShowAddClubModal(false)
+                onCloseModal(false)
             }
         })
+    }
+
+    const onUpdateClub = () => {
+        fetchUpdateClubData({
+            id: showAddClubModal.id,
+            ...clubInfo,
+            isPrivate: Number(clubInfo.isPrivate),
+        }).then((res) => {
+            if (res.result_code === 0) {
+                Toast.success("Successfuly updated post")
+                loadData()
+                setClubInfo({ title: "", isPrivate: false })
+                onCloseModal(false)
+            }
+        })
+    }
+
+    const onSubmitClub = () => {
+        if (!showAddClubModal.id) return
+
+        if (showAddClubModal.id === "add") {
+            onCreateClub()
+        } else {
+            onUpdateClub()
+        }
+    }
+
+    const onCloseModal = (isDeleteModal: boolean) => {
+        const _infoTemp = {
+            id: "",
+            isOpen: false,
+        }
+        if (!isDeleteModal) {
+            setShowAddClubModal(_infoTemp)
+        } else {
+            setShowWarningModal(_infoTemp)
+        }
     }
 
     const ClubItem = ({ item }: { item: clubInfo }) => {
@@ -125,13 +196,25 @@ export const Clubs = () => {
                             </View>
                             {tab === "my_clubs" && (
                                 <View style={[styles.clubBottomEditBlock, { gap: 15 }]}>
-                                    <Image style={styles.clubEditIcon} source={EditImg} />
-                                    <Image style={styles.clubEditIcon} source={TrashImg} tintColor="#ed1834" />
+                                    <TouchableOpacity
+                                        style={styles.iconWrapper}
+                                        onPress={() => {
+                                            setClubInfo({ title: item.title, isPrivate: item.private })
+                                            setShowAddClubModal({ id: item.id || "", isOpen: true })
+                                        }}>
+                                        <Image style={styles.clubEditIcon} source={EditImg} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.iconWrapper} onPress={() => setShowWarningModal({ isOpen: true, id: item.id || "" })}>
+                                        <Image style={styles.clubEditIcon} source={TrashImg} tintColor="#ed1834" />
+                                    </TouchableOpacity>
                                 </View>
                             )}
                         </View>
                     </View>
                 </View>
+                <TouchableOpacity style={[styles.iconWrapper, { position: "absolute", left: 5, top: 5 }]}>
+                    <Image style={styles.clubEditIcon} source={LockImg} />
+                </TouchableOpacity>
             </View>
         )
     }
@@ -171,13 +254,13 @@ export const Clubs = () => {
                 </View>
             </View>
             {tab === "my_clubs" && (
-                <TouchableOpacity onPress={() => setShowAddClubModal(true)} style={styles.addClubWrapper}>
+                <TouchableOpacity onPress={() => setShowAddClubModal({ id: "add", isOpen: true })} style={styles.addClubWrapper}>
                     <Image source={AddClubImg} style={{ width: 50, height: 50, objectFit: "contain" }} />
                 </TouchableOpacity>
             )}
-            <Modal popup animationType="slide-up" visible={showAddClubModal} onClose={() => setShowAddClubModal(false)} style={styles.modalWrapper} maskClosable>
+            <Modal popup animationType="slide-up" visible={showAddClubModal.isOpen} onClose={() => onCloseModal(false)} style={styles.modalWrapper} maskClosable>
                 <View>
-                    <Text style={{ textAlign: "center", fontSize: 20, fontWeight: "600" }}>Add Club</Text>
+                    <Text style={{ textAlign: "center", fontSize: 20, fontWeight: "600" }}>{showAddClubModal.id === "add" ? "Add Club" : "Update Club"} </Text>
                     <InputStyle inputTitle="Title">
                         <InputItem last type="text" style={styles.input} value={clubInfo.title} onChange={(e) => setClubInfo({ ...clubInfo, title: e })} />
                     </InputStyle>
@@ -185,16 +268,84 @@ export const Clubs = () => {
                         <Switch checked={clubInfo.isPrivate} onChange={(e) => setClubInfo({ ...clubInfo, isPrivate: e })} />
                     </InputStyle>
 
-                    <Button type="primary" style={styles.createBtn} onPress={() => onCreateClub()}>
-                        Create a club
+                    <Button type="primary" style={styles.createBtn} onPress={() => onSubmitClub()}>
+                        {showAddClubModal.id === "add" ? "Create a club" : "Update club"}
                     </Button>
                 </View>
+                {isLoading && <Loading />}
+            </Modal>
+            <Modal animationType="fade" transparent={false} visible={showWarningModal.isOpen} onClose={() => onCloseModal(true)} maskClosable style={styles.modalWrapperWarning}>
+                <View>
+                    <Text style={styles.tileWarningText}>Are you sure delete club ?</Text>
+                </View>
+                <View style={styles.warningBtns}>
+                    <Text style={[styles.btnText, styles.cancelText]} onPress={() => onCloseModal(true)}>
+                        No
+                    </Text>
+                    <Text style={[styles.btnText, { color: "#ed1834", fontWeight: "500" }]} onPress={() => onDeleteClub()}>
+                        Yes
+                    </Text>
+                </View>
+                {isLoading && <Loading />}
             </Modal>
         </Page>
     )
 }
 
 const styles = StyleSheet.create({
+    iconWrapper: {
+        paddingVertical: 12,
+        paddingHorizontal: 12,
+        borderRadius: 100,
+        backgroundColor: "#FFFFFF",
+        shadowColor: "rgba(0, 0, 0, 0.25)",
+        shadowOffset: {
+            width: 1,
+            height: 0,
+        },
+        shadowRadius: 100,
+        elevation: 10,
+        shadowOpacity: 1,
+    },
+    cancelText: {
+        borderRightWidth: 1,
+        borderStyle: "solid",
+        borderColor: "#0000001f",
+        fontWeight: "700",
+        color: "#0A78D6",
+    },
+    warningBtns: {
+        marginTop: 20,
+        alignItems: "center",
+        flexDirection: "row",
+        borderTopWidth: 1,
+        borderStyle: "solid",
+        borderTopColor: "#0000001f",
+    },
+    btnText: {
+        flex: 1,
+        justifyContent: "center",
+        paddingVertical: 16,
+        textAlign: "center",
+    },
+    tileWarningText: {
+        paddingHorizontal: 16,
+        color: "#212121",
+        fontSize: 17,
+        lineHeight: 18,
+        fontWeight: "700",
+    },
+    modalWrapperWarning: {
+        width: "90%",
+        paddingTop: 32,
+        marginHorizontal: 32,
+        borderRadius: 12,
+        backgroundColor: "#fff",
+        position: "absolute",
+        left: -10,
+        bottom: -Dimensions.get("window").height / 2,
+        zIndex: -1,
+    },
     clubBottomEditBlock: {
         flexDirection: "row",
         alignItems: "center",
