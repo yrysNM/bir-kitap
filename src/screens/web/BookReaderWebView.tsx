6fd2c7c6cfedc604337440e29286e721728d56ad
yrysNM <yrys.n3m@gmail.com>
2024-06-04 09:10:42 +0500

    fix(BookReaderWebView): change load method

--- src/screens/web/BookReaderWebView.tsx
+++ src/screens/web/BookReaderWebView.tsx
@@ -1,11 +1,15 @@
-import { useMemo, useRef } from "react"
+import { useMemo, useRef, useState } from "react"
 import { Header } from "../../components/Header"
 import { NoData } from "../../components/NoData"
 import { Page } from "../../layouts/Page"
 import { WebView } from "react-native-webview"
+import { useAppDispatch } from "../../hook/useStore"
+import { setLoading } from "../../redux/features/mainSlice"
 
 export const BookReaderWebView = ({ url = "https://clicklibrary.wordpress.com/wp-content/uploads/2018/09/it-by-stephen-king.pdf" }: { url?: string }) => {
+    const dispatch = useAppDispatch()
     const webViewEl = useRef<WebView>(null)
+    const [webviewKey, setWebviewKey] = useState<number>(0)
 
     const webUrl = useMemo(() => {
         if (url && url.length) {
@@ -16,10 +20,37 @@ export const BookReaderWebView = ({ url = "https://clicklibrary.wordpress.com/wp
     }, [url])
 
     return (
-        <Page>
+        <Page isFlatList>
             <Header isGoBack title="" />
             {webUrl.length ? (
-                <WebView ref={webViewEl} nestedScrollEnabled={true} source={{ uri: webUrl }} webviewDebuggingEnabled={true} ignoreSilentHardwareSwitch={true} javaScriptEnabled={true} style={{ height: "100%", width: "100%", backgroundColor: "#F7F9F6" }} onError={(e) => console.log(e)} />
+                <WebView
+                    key={webviewKey}
+                    ref={webViewEl}
+                    nestedScrollEnabled={true}
+                    source={{ uri: webUrl }}
+                    webviewDebuggingEnabled={true}
+                    ignoreSilentHardwareSwitch={true}
+                    javaScriptEnabled={true}
+                    style={{ height: "100%", width: "100%", backgroundColor: "#F7F9F6" }}
+                    onError={(e) => console.log(e)}
+                    onLoadEnd={(data) => {
+                        const { nativeEvent } = data
+                        const { title } = nativeEvent
+
+                        if (!title.trim()) {
+                            webViewEl.current?.stopLoading()
+                            webViewEl.current?.reload()
+                            setWebviewKey((webviewKey) => webviewKey + 1)
+                        }
+                    }}
+                    onLoadProgress={({ nativeEvent }) => {
+                        if (nativeEvent.progress !== 1) {
+                            dispatch(setLoading(true))
+                        } else {
+                            dispatch(setLoading(false))
+                        }
+                    }}
+                />
             ) : (
                 <NoData />
             )}
