dca5333138e0c6e650e6f659ad66d26d3a8b74c7
yrysNM <yrys.n3m@gmail.com>
2024-08-04 13:15:17 +0500

    chor: remove comments

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -14,7 +14,7 @@ export const Welcome = ({ isHaveNavigation = true }: { isHaveNavigation?: boolea
     useEffect(() => {
         const restTime = setTimeout(() => {
             if (navigation) {
-                navigation.navigate("Login")
+                navigation.navigate("BookCrossingWebView")
             }
         }, 1000)
 
@@ -23,50 +23,5 @@ export const Welcome = ({ isHaveNavigation = true }: { isHaveNavigation?: boolea
         }
     }, [])
 
-    return (
-        // <View style={styles.welcomeBlock}>
-        //     <Text style={styles.titleWelcome}>Feel the rhytm</Text>
-        //     <Text style={styles.descrWelcome}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Text>
-
-        //     <Button style={styles.welcomeBtn} onPress={() => navigation.navigate("CreateAccount")}>
-        //         <Text style={styles.btnText}>Get started!</Text>
-        //     </Button>
-        // </View>
-        <ImageBackground source={SplashImg} style={{ width: "100%", height: "100%" }} imageStyle={{ objectFit: "cover" }}></ImageBackground>
-    )
+    return <ImageBackground source={SplashImg} style={{ width: "100%", height: "100%" }} imageStyle={{ objectFit: "cover" }}></ImageBackground>
 }
-
-// const styles = StyleSheet.create({
-//     welcomeBlock: {
-//         marginTop: 106,
-//         paddingHorizontal: 16,
-//         position: "relative",
-//         flex: 1,
-//     },
-//     titleWelcome: {
-//         fontSize: 64,
-//         fontWeight: "700",
-//         lineHeight: 78,
-//         color: "#1E1E1E",
-//     },
-//     descrWelcome: {
-//         marginTop: 5,
-//         fontWeight: "500",
-//         fontSize: 14,
-//         lineHeight: 17,
-//     },
-//     welcomeBtn: {
-//         position: "absolute",
-//         bottom: 102,
-//         left: 16,
-//         width: "100%",
-//         backgroundColor: "#0C1E34",
-//         borderRadius: 19,
-//     },
-//     btnText: {
-//         color: "#fff",
-//         fontSize: 18,
-//         fontWeight: "400",
-//         lineHeight: 22,
-//     },
-// })
